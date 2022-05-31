193e90b69f67c31d91c3d113c7188245c2f9de7f
eladmallel <eladmallel@gmail.com>
2022-05-31 15:49:24 -0500

    cleanup

--- tasks/deploy.ts
+++ tasks/deploy.ts
@@ -42,10 +42,6 @@ task("deploy", "Deploys OtcEscrowApprovals")
         }
       }
 
-      network.chainId === 1
-        ? "0xa5409ec958c83c3f309868babaca7c86dcb077c1"
-        : "0xf57b2c51ded3a29e6891aba85459d600256cf317";
-
       const factory = await ethers.getContractFactory("OtcEscrowApprovals");
 
       const gasPrice = await getGasPriceWithPrompt(ethers);
